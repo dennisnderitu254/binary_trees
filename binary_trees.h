666345e261035da2b05ca6f6c76a7206c2ffbfbb
dennisnderitu254 <dknderitu@gmail.com>
2023-02-27 16:10:31 +0300

    Writing a function that inserts a node as the left-child of another node

--- binary_trees.h
+++ binary_trees.h
@@ -42,5 +42,6 @@ typedef struct levelorder_queue_s
 void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /*BINARY_TREES_H*/
