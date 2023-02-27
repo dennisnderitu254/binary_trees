85e16f9c72e710ede4ea426813c816c0cfa1ba5a
dennisnderitu254 <dknderitu@gmail.com>
2023-02-27 17:28:14 +0300

    linting code

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,18 +6,19 @@
  * @value: the value to put in the new node
  *
  * Return: a pointer to the new node, or NULL on failure
-
-**FUNCTIONALITY**
-1. Allocate memory for a new binary tree node.
-2. If the allocation fails, return NULL.
-3. Set the value of the new node to the value passed in.
-4. Set the parent of the new node to the parent passed in.
-5. Set the left child of the new node to NULL.
-6. Set the right child of the new node to NULL.
-7. Return a pointer to the new node.
-
-Time Complexity: O(1)
-*/
+ *
+ * FUNCTIONALITY
+ *
+ * 1. Allocate memory for a new binary tree node.
+ * 2. If the allocation fails, return NULL.
+ * 3. Set the value of the new node to the value passed in.
+ * 4. Set the parent of the new node to the parent passed in.
+ * 5. Set the left child of the new node to NULL.
+ * 6. Set the right child of the new node to NULL.
+ * 7. Return a pointer to the new node.
+ *
+ * Time Complexity: O(1)
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
