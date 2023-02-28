21a38f57aabe293b0e2e0d3efc7b609c88493e5e
dennisnderitu254 <dknderitu@gmail.com>
2023-02-28 11:26:30 +0300

    Betty linting Code

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,12 +5,11 @@
  * @node: Pointer to the node to check
  * Return: 1 if node is a leaf
  * Otherwise - returns 0
- */
-
-/**
- * FUNCTIONALITY*
+ *
+ * FUNCTIONALITY
  *
  * 1. If the node is NULL or the node has a left or right child, return 0.
+ *
  * 2. Otherwise, return 1.
  *
  * Time Complexity: O(1)
