5dc5c55979e0b9fae0f43ce4b800bd526b82eaf1
dennisnderitu254 <dknderitu@gmail.com>
2023-02-28 13:33:45 +0300

    Betty Linting Code

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -17,10 +17,10 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree && func)
-    {
-        binary_tree_postorder(tree->left, func);
-        binary_tree_postorder(tree->right, func);
-        func(tree->n);
-    }
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
 }
