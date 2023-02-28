62a7e4731bbf7a4fb2ff73e8583587bad38c1d68
dennisnderitu254 <dknderitu@gmail.com>
2023-02-28 17:15:04 +0300

    Writing a function that finds the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,16 +11,20 @@
  * FUNCTIONALITY *
  *
  * 1. If the node is NULL or the parent is NULL, return NULL.
- * 2. If the node is the left child of the parent, return the right child of the parent.
- * 3. If the node is the right child of the parent, return the left child of the parent.
+ *
+ * 2. If the node is the left child of the parent,
+ * return the right child of the parent.
+ *
+ * 3. If the node is the right child of the parent,
+ * return the left child of the parent.
  *
  * Time Complexity: O(1)
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
-    if (node->parent->left == node)
-        return (node->parent->right);
-    return (node->parent->left);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
