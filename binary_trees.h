7977be626c9da1cc7b729854f1da5595223df7a5
dennisnderitu254 <dknderitu@gmail.com>
2023-03-01 18:09:00 +0300

    No Implementation Functions Added

--- binary_trees.h
+++ binary_trees.h
@@ -87,5 +87,6 @@ avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif /*BINARY_TREES_H*/
