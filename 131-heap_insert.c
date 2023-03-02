5e988e0304f692251e060fae6461fe63f41bd596
dennisnderitu254 <dknderitu@gmail.com>
2023-03-02 15:19:32 +0300

    Working on Advanced tasks

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,10 +1,7 @@
 #include "binary_trees.h"
-#include <stdlib.h>
-#include <string.h>
-#define INIT_NODE           \
-    {                       \
-        0, NULL, NULL, NULL \
-    }
+#include<stdlib.h>
+#include<string.h>
+#define INIT_NODE {0, NULL, NULL, NULL}
 
 /**
  * swap - swaps two nodes in binary tree
@@ -14,45 +11,45 @@
  */
 bst_t *swap(bst_t *a, bst_t *b)
 {
-    bst_t a_copy = INIT_NODE;
+	bst_t a_copy = INIT_NODE;
 
-    a_copy.n = a->n;
-    a_copy.parent = a->parent;
-    a_copy.left = a->left;
-    a_copy.right = a->right;
-    a->parent = b;
-    a->left = b->left;
-    a->right = b->right;
-    if (b->left)
-        b->left->parent = a;
-    if (b->right)
-        b->right->parent = a;
+	a_copy.n = a->n;
+	a_copy.parent = a->parent;
+	a_copy.left = a->left;
+	a_copy.right = a->right;
+	a->parent = b;
+	a->left = b->left;
+	a->right = b->right;
+	if (b->left)
+		b->left->parent = a;
+	if (b->right)
+		b->right->parent = a;
 
-    b->parent = a_copy.parent;
-    if (a_copy.parent)
-    {
-        if (a == a_copy.parent->left)
-            a_copy.parent->left = b;
-        else
-            a_copy.parent->right = b;
-    }
-    if (b == a_copy.left)
-    {
-        b->left = a;
-        b->right = a_copy.right;
-        if (a_copy.right)
-            a_copy.right->parent = b;
-    }
-    else if (b == a_copy.right)
-    {
-        b->right = a;
-        b->left = a_copy.left;
-        if (a_copy.left)
-            a_copy.left->parent = b;
-    }
-    while (b->parent)
-        b = b->parent;
-    return (b);
+	b->parent = a_copy.parent;
+	if (a_copy.parent)
+	{
+		if (a == a_copy.parent->left)
+			a_copy.parent->left = b;
+		else
+			a_copy.parent->right = b;
+	}
+	if (b == a_copy.left)
+	{
+		b->left = a;
+		b->right = a_copy.right;
+		if (a_copy.right)
+			a_copy.right->parent = b;
+	}
+	else if (b == a_copy.right)
+	{
+		b->right = a;
+		b->left = a_copy.left;
+		if (a_copy.left)
+			a_copy.left->parent = b;
+	}
+	while (b->parent)
+		b = b->parent;
+	return (b);
 }
 
 /**
@@ -64,22 +61,21 @@ bst_t *swap(bst_t *a, bst_t *b)
  */
 char *convert(unsigned long int num, int base, int lowercase)
 {
-    static char *rep;
-    static char buffer[50];
-    char *ptr;
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
 
-    rep = (lowercase)
-              ? "0123456789abcdef"
-              : "0123456789ABCDEF";
-    ptr = &buffer[49];
-    *ptr = 0;
-    do
-    {
-        *--ptr = rep[num % base];
-        num /= base;
-    } while (num);
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = 0;
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num);
 
-    return (ptr);
+	return (ptr);
 }
 
 /**
@@ -89,10 +85,10 @@ char *convert(unsigned long int num, int base, int lowercase)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
@@ -102,39 +98,40 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 void insert(heap_t **root, heap_t *node)
 {
-    heap_t *tmp;
-    int size;
-    unsigned int i;
-    char *binary;
-    char c;
+	heap_t *tmp;
+	int size;
+	unsigned int i;
+	char *binary;
+	char c;
 
-    tmp = *root;
-    size = binary_tree_size(tmp) + 1;
-    binary = convert(size, 2, 1);
-    for (i = 1; i < strlen(binary); i++)
-    {
-        c = binary[i];
-        if (i == strlen(binary) - 1)
-        {
-            if (c == '1')
-            {
-                node->parent = tmp;
-                tmp->right = node;
-                break;
-            }
-            else if (c == '0')
-            {
-                node->parent = tmp;
-                tmp->left = node;
-                break;
-            }
-        }
-        if (c == '1')
-            tmp = tmp->right;
-        else if (c == '0')
-            tmp = tmp->left;
-    }
+	tmp = *root;
+	size = binary_tree_size(tmp) + 1;
+	binary = convert(size, 2, 1);
+	for (i = 1; i < strlen(binary); i++)
+	{
+		c = binary[i];
+		if (i == strlen(binary) - 1)
+		{
+			if (c == '1')
+			{
+				node->parent = tmp;
+				tmp->right = node;
+				break;
+			}
+			else if (c == '0')
+			{
+				node->parent = tmp;
+				tmp->left = node;
+				break;
+			}
+		}
+		if (c == '1')
+			tmp = tmp->right;
+		else if (c == '0')
+			tmp = tmp->left;
+	}
 }
+
 
 /**
  * heap_insert - inserts a value in Max Binary Heap
@@ -144,23 +141,23 @@ void insert(heap_t **root, heap_t *node)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *ht = NULL, *ret;
+	heap_t *ht = NULL, *ret;
 
-    if (!root)
-        return (NULL);
-    ht = calloc(1, sizeof(heap_t));
-    ht->n = value;
-    if (!*root)
-    {
-        *root = ht;
-        return (ht);
-    }
-    insert(root, ht);
-    while (ht->parent && ht->n > ht->parent->n)
-    {
-        ret = swap(ht->parent, ht);
-        if (ret)
-            *root = ret;
-    }
-    return (ht);
+	if (!root)
+		return (NULL);
+	ht = calloc(1, sizeof(heap_t));
+	ht->n = value;
+	if (!*root)
+	{
+		*root = ht;
+		return (ht);
+	}
+	insert(root, ht);
+	while (ht->parent && ht->n > ht->parent->n)
+	{
+		ret = swap(ht->parent, ht);
+		if (ret)
+			*root = ret;
+	}
+	return (ht);
 }
