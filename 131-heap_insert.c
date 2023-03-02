0b1edfcc05dcb479db14bcad7d574f344848f409
dennisnderitu254 <dknderitu@gmail.com>
2023-03-02 15:32:49 +0300

    Working on Advanced tasks

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,103 +1,181 @@
 #include "binary_trees.h"
-size_t bts(const binary_tree_t *tree);
-heap_t *heap_nf(heap_t *root, size_t number);
-heap_t *heap_r(heap_t *new, heap_t *parent);
 
 /**
- * heap_insert - heap inserter
- *
- * @tree: double pointer to root
- * @value: the value to store
- * Return: pointer to the created node
+ * heap_insert -  inserts a value into a Max Binary Heap
+ * @root: double pointer to the root node of the Heap
+ * @value: value store in the node to be inserted
+ * Return: pointer to the inserted node, or NULL on failure
+ * If the address stored in root is NULL,
+ * the created node must become the root node.
+ * Max Heap ordering
  */
-heap_t *heap_insert(heap_t **tree, int value)
+heap_t *heap_insert(heap_t **root, int value)
 {
-	size_t size;
-	int doswap = 1;
-	heap_t *new, *parent;
+	heap_t *aux = NULL, *new = NULL;
+	int balance = 0, aux_value = 0;
 
-	if (tree == NULL)
+	if (root == NULL)
 		return (NULL);
-
-	if (*tree == NULL)
+	if (*root == NULL)
 	{
-		new = binary_tree_node(NULL, value);
-		*tree = new;
-		return (new);
+		*root = binary_tree_node(NULL, value);
+		return (*root);
 	}
-
-	size = bts(*tree);
-	parent = heap_nf(*tree, (size - 1) / 2);
-	new = binary_tree_node(parent, value);
-	if (size % 2 == 1)
-		parent->left = new;
-	else
-		parent->right = new;
-	if (new == NULL)
-		return (NULL);
-
-	while (doswap == 1 && parent != NULL)
+	aux = *root;
+	while (aux->left && aux->right)
 	{
-		if (parent->n >= new->n)
-			doswap = 0;
+		balance = binary_tree_balance(aux);
+		if ((balance > 0 && binary_tree_is_perfect(aux->left) == 1) ||
+			(binary_tree_is_perfect(aux->right) == 0))
+			aux = aux->right;
 		else
-		{
-			new = heap_r(new, parent);
-			parent = new->parent;
-		}
+			aux = aux->left;
 	}
-
+	if (aux->left == NULL)
+	{
+		new = binary_tree_node(aux, value);
+		aux->left = new;
+	}
+	else
+	{
+		new = binary_tree_node(aux, value);
+		aux->right = new;
+	}
+	while (new->parent != NULL && new->n > new->parent->n)
+	{
+		aux_value = new->parent->n;
+		new->parent->n = new->n;
+		new->n = aux_value;
+		new = new->parent;
+	}
 	return (new);
 }
 
-/**
- * bts - measure the tree
- *
- * @tree: pointer to root
- * Return: size, otherwise 0
- */
+int balance_recursion(const binary_tree_t *tree, int count);
 
-size_t bts(const binary_tree_t *tree)
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: balance factor
+ * If tree is NULL, return 0
+ */
+int binary_tree_balance(const binary_tree_t *tree)
 {
+	int right_balance = 0;
+	int left_balance = 0;
+
 	if (tree == NULL)
 		return (0);
-	return (1 + bts(tree->left) + bts(tree->right));
+
+	if (tree->left != NULL)
+		left_balance = balance_recursion(tree->left, 1);
+	if (tree->right != NULL)
+		right_balance = balance_recursion(tree->right, 1);
+
+	return (left_balance - right_balance);
 }
 
 /**
- * heap_nf - heap node finder
- *
- * @root: pointer to the root
- * @number: index of the node to find
- * Return: pointer to the node
+ * balance_recursion - aux function for recursion
+ * @tree: pointer to tree
+ * @count: counter of height
+ * Return: height
  */
-heap_t *heap_nf(heap_t *root, size_t number)
+int balance_recursion(const binary_tree_t *tree, int count)
 {
-	size_t parentn, dir;
+	int count_r = 0;
+	int count_l = 0;
 
-	if (number == 0)
-		return (root);
+	if (tree->left != NULL)
+		count_l = balance_recursion(tree->left, count + 1);
 
-	parentn = (number - 1) / 2;
-	dir = (number - 1) % 2;
+	if (tree->right != NULL)
+		count_r = balance_recursion(tree->right, count + 1);
 
-	if (dir == 0)
-		return (heap_nf(root, parentn)->left);
-	return (heap_nf(root, parentn)->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (count);
+
+	if (count_r > count_l)
+		return (count_r);
+	else
+		return (count_l);
+}
+
+size_t height_recursion(const binary_tree_t *tree, size_t count);
+size_t leaves_recursion(const binary_tree_t *tree, size_t count);
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * A perfect binary tree is one that all internal nodes have two children
+ * and all leaves are at same level
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if perfect, 0 otherwise
+ * If tree is NULL, your function must return 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height = 0;
+	size_t leaves = 0;
+	size_t expected_leaves = 1;
+	size_t count;
+
+	if (tree == NULL)
+		return (0);
+
+	height = height_recursion(tree, 0);
+	leaves = leaves_recursion(tree, 0);
+
+	for (count = 0; count < height; count++)
+		expected_leaves = expected_leaves * 2;
+
+	if (expected_leaves == leaves)
+		return (1);
+
+	return (0);
 }
 
 /**
- * heap_r - heap rebalancer
- * @new: pointer to the new node
- * @parent: pointer to the new parent
- * Return: parent
+ * leaves_recursion - aux function for recursion
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * @count: Counter of leaves
+ * Return: Number of leaves
  */
-heap_t *heap_r(heap_t *new, heap_t *parent)
+size_t leaves_recursion(const binary_tree_t *tree, size_t count)
 {
-	int tmp;
+	if (tree->left != NULL)
+		count = leaves_recursion(tree->left, count);
 
-	tmp = new->n;
-	new->n = parent->n;
-	parent->n = tmp;
-	return (parent);
+	if (tree->right != NULL)
+		count = leaves_recursion(tree->right, count);
+
+	if (tree->right == NULL && tree->left == NULL)
+		return (count + 1);
+
+	return (count);
+}
+
+/**
+ * height_recursion - aux function for recursion
+ * @tree: pointer to tree
+ * @count: counter of height
+ * Return: height
+ */
+size_t height_recursion(const binary_tree_t *tree, size_t count)
+{
+	size_t count_r = 0;
+	size_t count_l = 0;
+
+	if (tree->left != NULL)
+		count_l = height_recursion(tree->left, count + 1);
+
+	if (tree->right != NULL)
+		count_r = height_recursion(tree->right, count + 1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (count);
+
+	if (count_r > count_l)
+		return (count_r);
+	else
+		return (count_l);
 }
